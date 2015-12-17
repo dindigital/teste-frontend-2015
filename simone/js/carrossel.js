691c6cc710236f89c21410888ebc7107bf96f841
Simone Myrrha <simyrrha@yahoo.com.br>
2015-12-17 01:13:10 -0200

    teste

---
+++ simone/js/carrossel.js
@@ -0,0 +1,10 @@
+$(function() {
+    $("#carrossel"). jCarouselLite({
+        btnPrev: '.prev', 
+        btnNext: '.next',
+        visible: 4
+    })
+
+     $("#carrossel").css('width', '1000px');
+})
+
