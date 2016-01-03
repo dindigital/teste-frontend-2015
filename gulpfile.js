ad1da31e4c548cae9422fb1548c353f37fb05487
Carlos Augusto <carlos.xell.3@gmail.com>
2016-01-03 17:55:24 -0200

    adição de imagens, arquivos renomeados e catalogados nas pastas de imagens de acordo com área a qual pertencem, criada função no arquivo do gulp para compilação do SASS, exclusão de códigos iniciais desnecessários

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // Código a ser executado.
+gulp.task('sass', function () {
+  gulp.src('./css/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./css/**/*.scss', ['sass']);
 });
