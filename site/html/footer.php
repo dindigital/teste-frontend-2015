63f52a1f86b7d690a8f422932ae829a13c06cece
Leandro Nunes <Macintosh@gnunesleandro.local>
2016-02-01 09:48:58 -0200

    Estrutura.

---
+++ site/html/footer.php
@@ -0,0 +1,4 @@
+<script src="library/js/jquery.1.11.3.min.js"></script>
+<script src="library/js/scripts.min.js"></script>
+</body>
+</html>
---
+++ site/html/index.php
@@ -0,0 +1,76 @@
+<?php include_once 'header.php'; ?>
+
+<main class="page-home">
+    <section class="carrossel">
+        <div class="container">
+            <div class="owl-carousel">
+                <div class="item"><img src="library/images/fullimage1.jpg" alt="The Last of us"></div>
+                <div class="item"><img src="library/images/fullimage2.jpg" alt="GTA V"></div>
+                <div class="item"><img src="library/images/fullimage3.jpg" alt="Mirror Edge"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="servicos">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-3 col-md-3">
+                    <h2><i class="fa fa-pencil fa-2x"></i><strong>Clean Theme</strong></h2>
+                    <p>Ut nec lorem id orci convallis porta. Donec pharetra neque eu velit dictum molestie. Duis porta gravida augue sed viverra. Quisque at nulla leo, non aliquet mi.</p>
+                    <span><strong>&gt;</strong>Read More</span>
+                </div>
+                <div class="col-xs-12 col-sm-3 col-md-3">
+                    <h2><i class="fa fa-pencil fa-2x"></i><strong>Clean Theme</strong></h2>
+                    <p>Ut nec lorem id orci convallis porta. Donec pharetra neque eu velit dictum molestie. Duis porta gravida augue sed viverra. Quisque at nulla leo, non aliquet mi.</p>
+                    <span><strong>&gt;</strong>Read More</span>
+                </div>
+                <div class="col-xs-12 col-sm-3 col-md-3">
+                    <h2><i class="fa fa-pencil fa-2x"></i><strong>Clean Theme</strong></h2>
+                    <p>Ut nec lorem id orci convallis porta. Donec pharetra neque eu velit dictum molestie. Duis porta gravida augue sed viverra. Quisque at nulla leo, non aliquet mi.</p>
+                    <span><strong>&gt;</strong>Read More</span>
+                </div>
+                <div class="col-xs-12 col-sm-3 col-md-3">
+                    <h2><i class="fa fa-pencil fa-2x"></i><strong>Clean Theme</strong></h2>
+                    <p>Ut nec lorem id orci convallis porta. Donec pharetra neque eu velit dictum molestie. Duis porta gravida augue sed viverra. Quisque at nulla leo, non aliquet mi.</p>
+                    <span><strong>&gt;</strong>Read More</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="ultimos-trabalhos">
+        <div class="container">
+            <div class="owl-carousel-works">
+                <div class="item"><img src="library/images/owl1.jpg" alt="The Last of us"></div>
+                <div class="item"><img src="library/images/owl2.jpg" alt="GTA V"></div>
+                <div class="item"><img src="library/images/owl3.jpg" alt="Mirror Edge"></div>
+                <div class="item"><img src="library/images/owl4.jpg" alt="The Last of us"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="depoimentos">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6">
+                    <h2>Testimonials</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut nulla sapien, at aliquam erat. Sed vitae massa tellus. Aliquam commodo aliquam metus, sed iaculis nibh tempus id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nec nisi in nisl euismod fringilla.</p>
+                    <p><strong>John Travis, CEO, DomainName.com</strong></p>
+                </div>
+
+                <div class="col-xs-12 col-sm-6 col-md-6">
+                    <h2>Our Clients</h2>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="well well-lg">This is a clean and modern, four column website PSD template. You can code it into a Wordpress website, HTML5 responsive website for your personal or client works. So ahead and download this wonderful PSD template! </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include_once 'footer.php'; ?>
